146dfde4ee4b6906a2c824173c4a7a47552b3083
JeanCharlesNeboit <jean_charles.neboit@icloud.com>
2019-01-22 16:34:24 +0100

    Initial Commit

---
+++ MovieWood/Movie.swift
@@ -0,0 +1,14 @@
+//
+//  Movie.swift
+//  MovieWood
+//
+//  Created by Jean-Charles Neboit on 22/01/2019.
+//  Copyright © 2019 Jean-Charles Neboit. All rights reserved.
+//
+
+import Foundation
+
+class Movie: Codable {
+    var id: Int
+    var poster: String
+}
